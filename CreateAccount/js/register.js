fba9a1cd5d68b0afa97b67749fa3ed4d71b8964b
TomBarone <93212351+TomBarone@users.noreply.github.com>
2022-02-01 15:37:48 -0500

    "Fixed the link"
    Supposably

--- CreateAccount/js/register.js
+++ CreateAccount/js/register.js
@@ -1,17 +1,17 @@
-const urlBase = 'http://superawesomecontactmanager3000.com/';
+const urlBase = 'http://superawesomecontactmanager3000.com/signUp.html';
 const extension = 'php';
 
 function createAccount()
 {
 	userId = 0;
 	document.getElementById("CreateError").innerHTML = "";
-	
+
 	var email = document.getElementById("CreateEmail").value;
 	var pass = document.getElementById("CreatePass").value;
 	var fName = document.getElementById("FirstName").value;
 	var lName = document.getElementById("LastName").value;
 	var confirmPass = document.getElementById("CreateConfirmPass").value;
-	
+
 	if(email == "")
 	{
 		document.getElementById("CreateError").innerHTML = "Please enter an email.";
@@ -19,7 +19,7 @@ function createAccount()
 	}
 	else
 	{
-		/* 
+		/*
 		test if email is valid if we wanna bother
 		if(!valid)
 		{
@@ -28,39 +28,39 @@ function createAccount()
 		}
 		*/
 	}
-	
+
 	if(pass != confirmPass)
 	{
 		document.getElementById("CreateError").innerHTML = "Passwords do not match.";
 		return;
 	}
-	
+
 	var jsonPayload = JSON.stringify({email:email,password:pass,firstName:fName,lastName:lName});
 	var url = urlBase + 'LAMPAPI/CreateAccount.' + extension;
-	
+
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	
+
 	try
 	{
 		xhr.send(jsonPayload);
-		
-		xhr.onreadystatechange = function() 
+
+		xhr.onreadystatechange = function()
 		{
-			if (this.readyState == 4 && this.status == 200) 
+			if (this.readyState == 4 && this.status == 200)
 			{
 				let jsonObject = JSON.parse( xhr.responseText );
-		
+
 				// this if statement could be could be wrong but I think its functional
 				if(jsonObject.error.length > 0)
-				{		
+				{
 					document.getElementById("CreateError").innerHTML = jsonObject.error;
 					return;
 				}
-		
+
 				document.getElementById("CreateError").innerHTML = "Account created successfully!";
-				
+
 				// force login after account creation can be removed if needed
 				login(email, pass);
 			}
