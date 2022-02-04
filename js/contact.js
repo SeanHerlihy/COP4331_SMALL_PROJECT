d4d2d09fc198629f80fe87947440d3f8eafe802a
Razgriz117 <martin.ds@knights.ucf.edu>
2022-02-03 19:35:55 -0500

    Added logout and readcookie to contact.js

--- js/contact.js
+++ js/contact.js
@@ -2,37 +2,39 @@ const urlBase = 'http://superawesomecontactmanager3000.com/';
 const extension = 'php';
 
 var userId = 0;
+let fName = "";
+let lName = "";
 
 function createContact(args)
 {
 	var tmp = {id:userId, firstName:args[1], lastName:args[2], address:args[3], email:args[4], phone:args[5]};
 	var jsonPayload = JSON.stringify(tmp);
 	var url = urlBase + 'LAMPAPI/addContact.' + extension;
-	
+
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	
+
 	try
 	{
 		xhr.send(jsonPayload);
-		
+
 		xhr.onreadystatechange() = function()
 		{
-			if (this.readyState == 4 && this.status == 200) 
+			if (this.readyState == 4 && this.status == 200)
 			{
 				let jsonObject = JSON.parse( xhr.responseText );
-		
-				
+
+
 				if(jsonObject.contactId < 0 || !jsonObject.hasOwnProperty('contactId')
-				{		
+				{
 					document.getElementById("contactError").innerHTML = "Error creating contact";
 					console.log(jsonObject.error);
 					return;
 				}
-		
+
 				document.getElementById("contactError").innerHTML = "Contact created successfully!";
-		
+
 				return jsonObject.contactId;
 			}
 		};
@@ -48,32 +50,32 @@ function editContact(args)
 	var tmp = {contactId:args[0], firstName:args[1], lastName:args[2], address:args[3], email:args[4], phone:args[5]};
 	var jsonPayload = JSON.stringify(tmp);
 	var url = urlBase + 'LAMPAPI/editContact.' + extension;
-	
+
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	
+
 	try
 	{
 		xhr.send(jsonPayload);
-		
+
 		xhr.onreadystatechange() = function()
 		{
-			if (this.readyState == 4 && this.status == 200) 
+			if (this.readyState == 4 && this.status == 200)
 			{
 				let jsonObject = JSON.parse( xhr.responseText );
-		
+
 				// should work
 				if(jsonObject.error.length > 0)
-				{		
+				{
 					document.getElementById("contactError").innerHTML = "Error updating contact";
 					console.log(jsonObject.error);
 					return;
 				}
-		
+
 				document.getElementById("contactError").innerHTML = "Contact updated successfully";
 				console.log("Contact updated successfully");
-				
+
 				return;
 			}
 		};
@@ -89,32 +91,32 @@ function deleteContact(contactId)
 	var tmp = {userId:userId, contactId:contactId};
 	var jsonPayload = JSON.stringify(tmp);
 	var url = urlBase + 'LAMPAPI/deleteContact.' + extension;
-	
+
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	
+
 	try
 	{
 		xhr.send(jsonPayload);
-		
+
 		xhr.onreadystatechange() = function()
 		{
-			if (this.readyState == 4 && this.status == 200) 
+			if (this.readyState == 4 && this.status == 200)
 			{
 				let jsonObject = JSON.parse( xhr.responseText );
-		
+
 				// should work
 				if(jsonObject.error.length > 0)
-				{		
+				{
 					document.getElementById("contactError").innerHTML = "Error deleting contact";
 					console.log(jsonObject.error);
 					return;
 				}
-		
+
 				document.getElementById("contactError").innerHTML = "Contact deleted successfully";
 				console.log("Contact deleted successfully");
-				
+
 				return;
 			}
 		};
@@ -125,21 +127,41 @@ function deleteContact(contactId)
 	}
 }
 
+function doLogout()
+{
+	document.cookie = "";
+	window.location.href = "index.html";
+}
 
+function readCookie()
+{
+	userId = -1;
+	let data = document.cookie;
+	let splits = data.split(",");
+	for(var i = 0; i < splits.length; i++)
+	{
+		let thisOne = splits[i].trim();
+		let tokens = thisOne.split("=");
+		if( tokens[0] == "FirstName" )
+		{
+			ftName = tokens[1];
+		}
+		else if( tokens[0] == "LastName" )
+		{
+			lName = tokens[1];
+		}
+		else if( tokens[0] == "UserId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	if( userId <= 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+		document.getElementById("UserName").innerHTML = "Logged in as " + fName + " " + lName;
+	}
+}
