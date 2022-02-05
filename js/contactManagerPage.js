e7aca9c3a2920850e8b19553e04534e94e331f68
Prexzer <60827526+Prexzer@users.noreply.github.com>
2022-02-04 23:47:29 -0500

    Add files via upload

--- js/contactManagerPage.js
+++ js/contactManagerPage.js
@@ -4,16 +4,17 @@ function insertEditIcons()
 {
     if (isInserted == false)
     {
+      var nameEditObj = document.getElementById("NameEditDiv");
       var phoneEditObj = document.getElementById("PhoneEditDiv");
       var emailEditObj = document.getElementById("EmailEditDiv");
       var birthEditObj = document.getElementById("BirthEditDiv");
       var cancelSaveDivObj = document.getElementById("CancelSaveButtonDiv");
 
+      nameEditObj.innerHTML +="<i id='NameEditIcon' class='fas fa-pen fa-2x editPenIcon'></i>";
       phoneEditObj.innerHTML +="<i id='PhoneEditIcon' class='fas fa-pen fa editPenIcon'></i>";
       emailEditObj.innerHTML +="<i id='EmailEditIcon' class='fas fa-pen fa editPenIcon'></i>";
       birthEditObj.innerHTML +="<i id='BirthEditIcon' class='fas fa-pen fa editPenIcon'></i>";
       cancelSaveDivObj.innerHTML +="<button type='submit' id = 'CancelEditButton' onclick='removeEditIcons();'>Cancel</button> <button type='submit' id = 'SaveEditButton'>Save</button>";
-
 
       isInserted = true;
     }
@@ -23,20 +24,23 @@ function removeEditIcons()
 {
   if (isInserted == true)
   {
+    var nameEditObj = document.getElementById("NameEditIcon");
     var phoneEditObj = document.getElementById("PhoneEditIcon");
     var emailEditObj = document.getElementById("EmailEditIcon");
     var birthEditObj = document.getElementById("BirthEditIcon");
     var cancelEditBtn = document.getElementById("CancelEditButton");
     var saveEditBtn = document.getElementById("SaveEditButton");
 
+    nameEditObj.remove();
     phoneEditObj.remove();
     emailEditObj.remove();
     birthEditObj.remove();
     cancelEditBtn.remove();
     saveEditBtn.remove();
+
+    isInserted = false;
   }
 
-  isInserted = false;
 }
 
 function appendUserContactsToSideBar()
