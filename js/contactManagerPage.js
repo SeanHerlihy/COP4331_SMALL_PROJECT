84d80ae26baeffb9f578ec596f2eb1c393db3505
Prexzer <60827526+Prexzer@users.noreply.github.com>
2022-02-04 23:45:42 -0500

    Add files via upload

--- js/contactManagerPage.js
+++ js/contactManagerPage.js
@@ -1,21 +1,19 @@
-console.log("LOADED");
 var isInserted = false;
 
 function insertEditIcons()
 {
     if (isInserted == false)
     {
-      var nameEditObj = document.getElementById("NameEditDiv");
       var phoneEditObj = document.getElementById("PhoneEditDiv");
       var emailEditObj = document.getElementById("EmailEditDiv");
       var birthEditObj = document.getElementById("BirthEditDiv");
       var cancelSaveDivObj = document.getElementById("CancelSaveButtonDiv");
 
-      nameEditObj.innerHTML +="<i id='NameEditIcon' class='fas fa-pen fa-2x editPenIcon'></i>";
       phoneEditObj.innerHTML +="<i id='PhoneEditIcon' class='fas fa-pen fa editPenIcon'></i>";
       emailEditObj.innerHTML +="<i id='EmailEditIcon' class='fas fa-pen fa editPenIcon'></i>";
       birthEditObj.innerHTML +="<i id='BirthEditIcon' class='fas fa-pen fa editPenIcon'></i>";
       cancelSaveDivObj.innerHTML +="<button type='submit' id = 'CancelEditButton' onclick='removeEditIcons();'>Cancel</button> <button type='submit' id = 'SaveEditButton'>Save</button>";
+
 
       isInserted = true;
     }
@@ -25,34 +23,36 @@ function removeEditIcons()
 {
   if (isInserted == true)
   {
-    var nameEditObj = document.getElementById("NameEditIcon");
     var phoneEditObj = document.getElementById("PhoneEditIcon");
     var emailEditObj = document.getElementById("EmailEditIcon");
     var birthEditObj = document.getElementById("BirthEditIcon");
     var cancelEditBtn = document.getElementById("CancelEditButton");
     var saveEditBtn = document.getElementById("SaveEditButton");
 
-    nameEditObj.remove();
     phoneEditObj.remove();
     emailEditObj.remove();
     birthEditObj.remove();
     cancelEditBtn.remove();
     saveEditBtn.remove();
-
-    isInserted = false;
   }
 
+  isInserted = false;
 }
 
-function appendUserContactsToSideBar(username)
+function appendUserContactsToSideBar()
 {
-	let htmlString = `<div id = "${username}" class ="img-responsive userContactSideBarDiv" onclick = "displayContactInfo();">
+  // firstName, lastName
+  //let username = firstName + " " + lastName;
+
+  let username = "patrick";
+
+	let htmlString = `<button id = "${username}" class ="img-responsive userContactSideBarDiv" onclick = "displayContactInfo(${username});">
 	<img src = "https://i.ibb.co/n6ps4Cx/l60Hf.png" id = "profilePicture">
-	<p id = "contactFullName"> Billy Smith </p>
-    </div>`;
+	<p id = "contactFullName"> ${username} </p>
+    </button>`;
 
-	let sideBarDiv = Document.getElementById("scroll-bar");
+	let sideBarDiv = document.getElementById("scroll-bar");
 
-	sideBarDiv += htmlString;
+	sideBarDiv.innerHTML += htmlString;
 
 }
