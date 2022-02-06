103f981a9610e14ba59373c2e5946369b05f2be9
aribelruiz <aribelruiz@knights.ucf.edu>
2022-02-06 00:58:01 -0500

    Attempted Merge CM CSS

--- js/contactManagerPage.js
+++ js/contactManagerPage.js
@@ -9,7 +9,7 @@ function insertEditIcons(isInsertedBool)
       var birthEditObj = document.getElementById("BirthEditDiv");
       var cancelSaveDivObj = document.getElementById("CancelSaveButtonDiv");
 
-      nameEditObj.innerHTML ="<button id='NameEditIcon' class='fas fa-pen fa-2x editPenIcon'></button>";
+      nameEditObj.innerHTML =`<button id='NameEditIcon' onclick="editInfo('FirstName', false); editInfo('LastName', false)" class='fas fa-pen fa-2x editPenIcon'></button>`;
       phoneEditObj.innerHTML =`<button id='PhoneEditIcon' onclick="editInfo('Phone', false)" class='fas fa-pen fa-sm editPenIcon'></button>`;
       emailEditObj.innerHTML =`<button id='EmailEditIcon' onclick="editInfo('Email', false)" class='fas fa-pen fa-sm editPenIcon'></button>`;
       birthEditObj.innerHTML =`<button id='BirthEditIcon' onclick="editInfo('Birth', false)" class='fas fa-pen fa-sm editPenIcon'></button>`;
@@ -30,21 +30,34 @@ function editInfo(infoType, alreadyClickedBool)
 
   if (alreadyClickedBool == false)
   {
-    infoDiv.innerHTML += `<input type='text' id = 'input${infoType}' placeholder=' Please enter new info'>`;
-    document.getElementById(`${infoType}EditDiv`).innerHTML = `<button id='${infoType}EditIcon' onclick="editInfo('${infoType}', true)" class='fas fa-pen fa-sm editPenIcon'></button>`;
+    if (infoType == "FirstName" || infoType == "LastName")
+    {
+      infoDiv.innerHTML += `<input type='text' id = 'input${infoType}' placeholder=' Please enter ${infoType}' title='Enter ${infoType}'>`;
+      document.getElementById(`NameEditDiv`).innerHTML = `<button id='NameEditIcon' onclick="editInfo('${infoType}', true)" class='fas fa-pen fa-2x editPenIcon'></button>`;
+    }
+    else
+    {
+      infoDiv.innerHTML += `<input type='text' id = 'input${infoType}' placeholder=' Please enter ${infoType}'>`;
+      document.getElementById(`${infoType}EditDiv`).innerHTML = `<button id='${infoType}EditIcon' onclick="editInfo('${infoType}', true)" class='fas fa-pen fa-sm editPenIcon'></button>`;
+    }
   }
-  
+
 }
 
 function removeEditIcons()
 {
-  
     var nameEditObj = document.getElementById("NameEditIcon");
     var phoneEditObj = document.getElementById("PhoneEditIcon");
     var emailEditObj = document.getElementById("EmailEditIcon");
     var birthEditObj = document.getElementById("BirthEditIcon");
     var cancelEditBtn = document.getElementById("CancelEditButton");
     var saveEditBtn = document.getElementById("SaveEditButton");
+
+    var firstNameTextBox = document.getElementById("inputFirstName")
+    var firstNameInfo = document.getElementById("FirstNameInfoText");
+
+    var lastNameTextBox = document.getElementById("inputLastName")
+    var lastNameInfo = document.getElementById("LastNameInfoText");
 
     var phoneTextBox = document.getElementById("inputPhone");
     var phoneInfo = document.getElementById("PhoneInfoText");
@@ -55,7 +68,6 @@ function removeEditIcons()
     var birthTextBox = document.getElementById("inputBirth");
     var birthInfo = document.getElementById("BirthInfoText");
 
-
     // removes all edit icons
     nameEditObj.remove();
     phoneEditObj.remove();
@@ -64,8 +76,13 @@ function removeEditIcons()
     cancelEditBtn.remove();
     saveEditBtn.remove();
 
-
     // removes input boxes
+    if (firstNameTextBox != null)
+      firstNameTextBox.remove();
+
+    if (lastNameTextBox != null)
+      lastNameTextBox.remove();
+
     if (phoneTextBox != null)
       phoneTextBox.remove();
 
@@ -73,38 +90,16 @@ function removeEditIcons()
       emailTextBox.remove();
 
     if (birthTextBox != null)
-    birthTextBox.remove();
+      birthTextBox.remove();
 
+    firstNameInfo.style.display="block";
+    lastNameInfo.style.display="block";
     phoneInfo.style.display="block";
     emailInfo.style.display="block";
     birthInfo.style.display="block";
 
-    document.getElementById('EditDeleteHeader').firstChild =  '<button class="clickableAwesomeFont" id="EditButton" title="Edit Contact" alt="edit contact icon" onclick="insertEditIcons(false)"> <i class="far fa-edit fa-3x"></i> </button>'
-  
-
+    document.getElementById('EditDeleteHeader').firstChild =  '<button class="clickableAwesomeFont" id="EditButton" title="Edit Contact" alt="edit contact icon" onclick="insertEditIcons(false)"> <i class="far fa-edit fa-3x"></i> </button>';
 }
-
-// adds a div to the side bar with a user's picture and name
-function appendUserContactsToSideBar()
-{
-  // firstName, lastName
-
-  let firstName = "Mr.";
-  let lastName = "Patrick";
-
-  let username = firstName + " " + lastName;
-
-	let htmlString = `<button id = '${username}' class ="img-responsive userContactSideBarDiv" onclick = "displayContactInfo('${firstName}', '${lastName}');">
-	<img src = "https://i.ibb.co/n6ps4Cx/l60Hf.png" id = "profilePicture">
-	<p id = "contactFullName"> ${username} </p>
-    </button>`;
-
-	let sideBarDiv = document.getElementById("scroll-bar");
-
-	sideBarDiv.innerHTML += htmlString;
-
-}
-
 
 // clicking a contact on the side bar will pull from the hashMap and display a user's info
 function displayContactInfo(firstName, lastName)
@@ -116,10 +111,16 @@ function displayContactInfo(firstName, lastName)
    </h1>
    <div id="top-info">
      <img id="profile-pic" src="https://i.ibb.co/QbzfxWp/relaxing-cat-1.jpg" alt="">
-     <h2 id="profileNameFirst">${firstName}</h2>
-     <h2 id = "profileNameSecond"> ${lastName}</h2>
-     <div id="NameEditDiv" class="editIconDiv">
-     </div>
+     <div id= "NameDiv">
+       <div id="FirstNameDiv">
+         <h2 id="FirstNameInfoText">${firstName}</h2>
+       </div>
+       <div id="LastNameDiv">
+         <h2 id = "LastNameInfoText"> ${lastName}</h2>
+       </div>
+       <div id="NameEditDiv" class="editIconDiv">
+       </div>
+    </div>
    </div>
    <hr class = white-page-line>
    <div id="secondary-info">
@@ -154,54 +155,4 @@ function displayContactInfo(firstName, lastName)
   let displayScreen = document.getElementById("inner-screen");
   displayScreen.lastElementChild.remove();
   displayScreen.innerHTML += htmlString;
-
-}
-
-function displayCreateUserPage()
-{
-  let htmlString = `<div id=CreateAccountDiv>
-  <div id="top-info">
-    <img id="create-profile-pic" src="https://i.ibb.co/n6ps4Cx/l60Hf.png" alt="">
-    <h2 id="profile-name"></h2>
-  </div>
-  <div id="secondary-info">
-    <div class="inputContainer">
-      <div id = "containsNames">
-        <h3 class="fieldText"> First Name: </h3>
-        <input type="text" id = "FirstName" placeholder = "First Name" class = "round-borders">
-        <h3 class="fieldText"> Last Name: </h3>
-        <input type="text" id = "LastName" placeholder = "Last Name" class = "round-borders">
-      </div>
-      <h3 class="fieldText"> E-mail Address: </h3>
-      <input type="text" id = "Email" placeholder = "E-mail" class = "round-borders">
-      <h3 class="fieldText"> Phone Number: </h3>
-      <input type="text" id = "PhoneNumber" placeholder = "Phone #" class = "round-borders">
-      <h3 class="fieldText"> Date of Birth (mm/dd/yyyy): </h3>
-      <input type="text" id = "Birthday" placeholder = "mm/dd/yyyy" class = "round-borders">
-      <div id = "contains-buttons">
-        <button type="submit" id = "CancelButton">Cancel</button>
-        <button type="submit" id = "CreateButton">Create</button>
-      </div>
-    </div>
-  </div>
-</div>`;
-
-  let displayScreen = document.getElementById("inner-screen");
-  displayScreen.lastElementChild.remove();
-  displayScreen.innerHTML += htmlString;
-}
-
-function displayMainWelcomeScreen()
-{
-
-  let displayScreen = document.getElementById("inner-screen");
-  displayScreen.lastElementChild.remove();
-  displayScreen.innerHTML += `<div id="info">
-  <div id = "WelcomeInsideDiv">
-    <p id = "WelcomeMSG">Welcome back to SUPER AWESOME CONTACT MANAGER 3000</p>
-    <p id = "welcomeDivUserName"> @Person Logged-in!!!!</p>
-    <img src="https://i.ibb.co/QbzfxWp/relaxing-cat-1.jpg" id = WelcomePic alt ="https://i.ibb.co/vzWSRXY/Screenshot-594.png"></img>
-  </div>
-</div>`;
-  
 }
