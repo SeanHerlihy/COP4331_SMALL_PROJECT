55fe258fb43f5928221014c3676b1a6815648932
TomBarone <93212351+TomBarone@users.noreply.github.com>
2022-02-07 10:42:41 -0500

    Changed the logo to martins logo
    Changed the cm welcome page to martins logo

--- js/contactManagerPage.js
+++ js/contactManagerPage.js
@@ -279,7 +279,7 @@ function displayMainWelcomeScreen()
   <div id = "WelcomeInsideDiv">
     <p id = "WelcomeMSG">Welcome back to SUPER AWESOME CONTACT MANAGER 3000</p>
     <p id = "welcomeDivUserName"> @Person Logged-in!!!!</p>
-    <img src="https://i.ibb.co/QbzfxWp/relaxing-cat-1.jpg" id = WelcomePic alt ="https://i.ibb.co/vzWSRXY/Screenshot-594.png"></img>
+    <img src="https://i.ibb.co/VwpYLdc/Shitty-Logo2-Transparent.png" id = WelcomePic alt ="https://i.ibb.co/vzWSRXY/Screenshot-594.png"></img>
   </div>
 </div>`;
 
